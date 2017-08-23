4f4410aa6a6b8d5cd174a60b3c377a84e7e58ba5
Alexandra Khegay <khaleksa@gmail.com>
2017-08-23 16:27:08 +0500

    get top twitter hashtags by twitter api

--- lib/api/twitter.rb
+++ lib/api/twitter.rb
@@ -4,16 +4,60 @@ module Api
   class Twitter
     include HTTParty
 
+    DEFAULT_WOEID = 1
+    TWITTER_HOST = 'https://twitter.com'
+    SEARCH_RESULT_LIMIT = 100
+
+    base_uri 'https://api.twitter.com'
+
     def initialize
       @config = ::Rails.configuration.twitter
+      self.class.headers 'Authorization' => " Bearer #{get_token}"
     end
 
     def search(search_text)
-      headers = { 'Authorization' => " Bearer #{get_token}" }
-      params = { q: CGI.escape(search_text) }
-      result = self.class.get(@config.api[:search_url], headers: headers, query: params )
+      params = {
+          q: CGI.escape(search_text),
+          count: SEARCH_RESULT_LIMIT
+      }
+      result = self.class.get(@config.api[:search_path], query: params)
       data = JSON.parse(result.body)
-      data['statuses']
+
+      tweets = []
+      data['statuses'].each do |tt|
+        user_info = tt['user']
+        tweet_data = HashWithIndifferentAccess.new({
+            text: tt['text'],
+            created_at: tt['created_at'],
+            user: {
+              name: user_info['name'],
+              avatar: user_info['profile_image_url'],
+              url: "#{TWITTER_HOST}/#{user_info['screen_name']}",
+              favorite_count: user_info['favorite_count'].to_i,
+              retweet_count: user_info['retweet_count'].to_i
+            }
+        })
+        tweets<<tweet_data
+      end
+      tweets
+    end
+
+    def trends(location_id = DEFAULT_WOEID)
+      params = { id: location_id }
+      result = self.class.get(@config.api[:trends_path], query: params)
+      data = JSON.parse(result.body)
+
+      hashtags = []
+      data.first['trends'].each do |trend|
+        next if trend['name'].blank? || !is_hashtag?(trend['name']) || trend['tweet_volume'].blank?
+        trend_info = HashWithIndifferentAccess.new({
+          name: trend['name'],
+          url: trend['url'],
+          volume: trend['tweet_volume']
+        })
+        hashtags<<trend_info
+      end
+      hashtags.sort{ |a, b| b[:volume]<=>a[:volume] }.take(10)
     end
 
     private
@@ -25,7 +69,7 @@ module Api
           'Authorization' => "Basic #{credentials}",
           'Content-Type' => 'application/x-www-form-urlencoded;charset=UTF-8'
       }
-      result = self.class.post(@config.api[:token_url], headers: headers, body: 'grant_type=client_credentials' )
+      result = self.class.post(@config.api[:token_path], headers: headers, body: 'grant_type=client_credentials' )
       data = JSON.parse(result.body)
       @token = data['access_token']
     end
@@ -35,6 +79,10 @@ module Api
       key = CGI.escape(@config.api[:key])
       secret = CGI.escape(@config.api[:secret])
       @bearer_token_credentials = Base64.strict_encode64("#{key}:#{secret}")
+    end
+
+    def is_hashtag?(str)
+      str.match('^#\w+$').present?
     end
   end
 end
--- spec/lib/api/twitter_spec.rb
+++ spec/lib/api/twitter_spec.rb
@@ -7,24 +7,38 @@ describe Api::Twitter do
   let(:config) { Rails.configuration.twitter }
   let(:token) { 'SomeTokenValue' }
 
-  def get_stubbed_request
+  def get_stubbed_request(url, fixture_name)
     allow_any_instance_of(Api::Twitter).to receive(:get_token).and_return(token)
 
-    fixture = File.read(File.expand_path("fixtures/twitter_response.json", File.dirname(__FILE__)))
+    fixture = File.read(File.expand_path("fixtures/#{fixture_name}.json", File.dirname(__FILE__)))
     headers = { 'Authorization' => "Bearer #{token}" }
-    params = { q: CGI.escape(search_text) }
-    stub_request(:get, config.api[:search_url])
+    stub_request(:get, url)
         .with(query: params, headers: headers)
         .to_return(body: fixture, status: 200, headers: {})
   end
 
+  before do
+    get_stubbed_request(request_url, result_fixture)
+  end
+
   describe '#search' do
+    let(:request_url) { "https://api.twitter.com#{config.api[:search_path]}" }
+    let(:result_fixture) { 'twitter_search' }
+    let(:params) {{ q: CGI.escape(search_text), count: 100 }}
     let(:search_text) { '@rails' }
-    let!(:stubbed_request) { get_stubbed_request }
 
     it 'should return array of tweets for searched text' do
       expect(subject.search(search_text).size).to eq(15)
     end
+  end
 
+  describe '#trends' do
+    let(:request_url) { "https://api.twitter.com#{config.api[:trends_path]}" }
+    let(:result_fixture) { 'twitter_trends' }
+    let(:params) {{ id: 1 }}
+
+    it 'should return top 10 hashtags' do
+      expect(subject.trends.size).to eq(10)
+    end
   end
 end
