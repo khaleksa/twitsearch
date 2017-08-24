22b7c71684792186fdf09ea4c4d745db3b65b2a3
Alexandra Khegay <khaleksa@gmail.com>
2017-08-24 19:22:17 +0500

    cache results of trends & search requests

--- lib/twitter_provider.rb
+++ lib/twitter_provider.rb
@@ -1,11 +1,15 @@
 class TwitterProvider
   class << self
     def search(search_text)
-      TwitterApi::Search.new(config[:search_path], oauth_token, search_text).perform
+      Rails.cache.fetch("twitter_api/search/#{Digest::MD5.hexdigest(search_text)}", expires_in: 5.minute) do
+        TwitterApi::Search.new(config[:search_path], oauth_token, search_text).perform
+      end
     end
 
     def trends
-      TwitterApi::Trends.new(config[:trends_path], oauth_token).perform
+      Rails.cache.fetch('twitter_api/trends', expires_in: 5.minute) do
+        TwitterApi::Trends.new(config[:trends_path], oauth_token).perform
+      end
     end
 
     private
